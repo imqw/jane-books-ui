07cca289be560afb0e5078f454799ba5c879c980
qiuwei <Wcbc0310@126.com>
2021-01-28 14:53:32 +0800

    添加element组件配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import ElementUI from 'element-ui';
 import HelloWorld from '@/components/HelloWorld'
+import IndexVue from '@/components/IndexVue'
 
 Vue.use(Router)
+Vue.use(ElementUI)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'IndexVue',
+      component: IndexVue
     }
   ]
 })
